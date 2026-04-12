2ecefa87d5db457b2010aa18cb4c1187b47b3fa3
Diego Mauricio Pacheco Lima <diegom.pacheco.jktic@gmail.com>
2026-04-11 19:57:31 -0500

    Curso Spring security : Memory user admin created

--- src/main/java/com/dp/pizza/web/config/SecurityConfig.java
+++ src/main/java/com/dp/pizza/web/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,13 +24,29 @@ public class SecurityConfig {
                         .anyRequest()
                         //.permitAll()
                         .authenticated()
-                        .requestMatchers(HttpMethod.GET, "/api/*").permitAll() //No auth
-                        .requestMatchers(HttpMethod.GET, "/api/**").permitAll() // No auth
+                        //.requestMatchers(HttpMethod.GET, "/api/*").permitAll() //No auth
+                        //.requestMatchers(HttpMethod.GET, "/api/**").permitAll() // No auth
 
                 ).httpBasic(Customizer.withDefaults())
                 .csrf(csrf -> csrf.disable())
-                .cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults());
 
         return httpSecurity.build();
+    }
+
+
+    @Bean
+    public UserDetailsService memoryUsers(){
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 }
